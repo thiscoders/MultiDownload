fb42daa373a772e2dbf5836fa2bbeadf397595de
ye <1621963477@qq.com>
2016-10-05 21:40:10 +0800

    多线程断点下载完成！

--- src/com/lgiue/download/Downloader.java
+++ src/com/lgiue/download/Downloader.java
@@ -2,10 +2,7 @@ package com.lgiue.download;
 
 import com.lgiue.utils.DUtils;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.RandomAccessFile;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -27,7 +24,6 @@ public class Downloader {
     private int runningThread;
 
     private String destFilePath;
-    private String destTempPath;
     /**
      *
      * @param urls  下载资源的url
@@ -44,7 +40,6 @@ public class Downloader {
         //使当前运行线程等于总线程数
         this.runningThread=threadCount;
         this.destFilePath=DUtils.getFileTitleAndPath(this.downPath,this.urls);
-        this.destTempPath=DUtils.getFileTitleAndPath(this.tempPath,this.urls);
     }
 
     /**
@@ -67,7 +62,10 @@ public class Downloader {
 
             File dir=new File(this.downPath);
             if(!dir.exists()){
-                dir.mkdirs();
+                boolean res=dir.mkdirs();
+                if(!res){
+                    System.out.println("下载目录创建失败！");
+                }
             }
 
             //在本地创建下载文件的镜像
@@ -98,11 +96,13 @@ public class Downloader {
         private int threadID;
         private int istart;
         private int iend;
+        private String destTempPath;
 
-        public downThread(int threadID,int istart,int iend){
+        private downThread(int threadID,int istart,int iend){
             this.threadID=threadID;
             this.istart=istart;
             this.iend=iend;
+            this.destTempPath=DUtils.getFileTitleAndPath(Downloader.this.tempPath,threadID+".txt");
         }
 
         @Override
@@ -112,6 +112,18 @@ public class Downloader {
                 HttpURLConnection connection= (HttpURLConnection) url.openConnection();
                 connection.setRequestMethod("GET");
                 connection.setConnectTimeout(5000);
+
+                File item=new File(this.destTempPath);
+                if(item.exists()&&item.length()!=0){
+                    BufferedReader reader=new BufferedReader(new FileReader(item));
+                    String last=reader.readLine();
+                    if(!last.isEmpty()){
+                        this.istart=Integer.parseInt(last);
+                    }
+                    reader.close();
+                }
+
+
                 connection.addRequestProperty("Range","bytes="+this.istart+"-"+this.iend);
 
                 int resCode=connection.getResponseCode();
@@ -120,17 +132,47 @@ public class Downloader {
                     return;
                 }
 
+
                 //获取inputstream
                 InputStream stream=connection.getInputStream();
                 RandomAccessFile part=new RandomAccessFile(Downloader.this.destFilePath,"rw");
                 part.seek(istart);
 
-                int len=-1;
+                File tdir=new File(Downloader.this.tempPath);
+                if(!tdir.exists()){
+                    boolean res=tdir.mkdirs();
+                    if(!res){
+                        System.out.println("缓存目录创建失败！");
+                    }
+                }
+                int totle=0;
+
+                int len;
                 byte[] buffer=new byte[1024*1024];
                 while ((len=stream.read(buffer))!=-1){
                     part.write(buffer,0,len);
-                }
+                    totle+=len;
 
+                    RandomAccessFile temp=new RandomAccessFile(this.destTempPath,"rwd");
+                    int last=this.istart+totle;
+                    temp.write(String.valueOf(last).getBytes());
+                    temp.close();
+                }
+                System.out.println("线程"+this.threadID+"下载完成！");
+                synchronized (this){
+                    Downloader.this.runningThread--;
+                    if(Downloader.this.runningThread==0){
+                        for(int i=0;i<Downloader.this.threadCount;i++){
+                            String tpath=DUtils.getFileTitleAndPath(Downloader.this.tempPath,i+".txt");
+                            File tfile=new File(tpath);
+                            if(tfile.exists()){
+                                //删除缓存文件
+                                //tfile.delete();
+                                System.out.println("去掉注释，删除文件！");
+                            }
+                        }
+                    }
+                }
             } catch (MalformedURLException e) {
                 e.printStackTrace();
             } catch (IOException e) {
--- src/com/lgiue/main/MainFunc.java
+++ src/com/lgiue/main/MainFunc.java
@@ -7,10 +7,10 @@ import com.lgiue.download.Downloader;
  * Created by ye on 10/4/16.
  */
 public class MainFunc {
-    private static String urls="http://127.0.0.1:8080/share/windowsdo/apink.mp4";
+    private static String urls="http://127.0.0.1:8080/share/linuxdo/android.zip";
     private static String tempPath="/home/ye/Downloads/kaifa/temp/";
     private static String downPath="/home/ye/Downloads/kaifa/";
-    private static int threadCount=5;
+    private static int threadCount=3;
 
     public static void main(String[] args){
         Downloader downloader=new Downloader(urls,tempPath,downPath,threadCount);
