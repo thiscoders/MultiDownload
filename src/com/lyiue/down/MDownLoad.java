10100dd1633931583d4107b454c3e0d4d99221ca
ye <1621963477@qq.com>
2016-10-05 14:50:22 +0800

    Initial commit

---
+++ src/com/lyiue/down/MDownLoad.java
@@ -0,0 +1,12 @@
+package com.lyiue.down;
+
+/**
+ * Created by ye on 10/4/16.
+ */
+public class MDownLoad {
+
+    public static void main(String[] args){
+        System.out.println("多线程断点下载！");
+    }
+
+}
